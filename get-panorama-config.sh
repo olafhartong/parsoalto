6cf8c0777570bfb4b28081e3240e005e1525ec57
Olaf Hartong <olaf@splitbrains.eu>
2014-07-15 08:14:08 +0200

    added-used-rule-support

--- get-panorama-config.sh
+++ get-panorama-config.sh
@@ -37,11 +37,16 @@ echo "==================================================="
 echo "                  getting configs"
 echo "==================================================="
    for host in $vsys; do
+######### In panorama unused rules need to be generated so generating them now polling them after the rest of the run per host so it will be done to pull.
+      echo "Requesting $host/unused/rules"
+      unused=$(curl -s -k "https://$panorama/api/?type=op&cmd=<show><rule-use><type>used</type><rule-base>security</rule-base><device-group>$host</device-group></rule-use></show>&key=$key" | xmllint --format --recover - | grep '<job>' | tr \<\/job\> " " | tr -d ' ')
     for url in $config; do
       fileurl=`echo $url | sed 's@/@-@g'`
       echo "Fetching $host/$url"
       curl -s -k "https://$panorama/api/?type=config&action=get&key=$key&xpath=/config/devices/entry\[@name='localhost.localdomain'\]/device-group/entry\[@name='$host'\]/$url" | xmllint --format --recover -  > $location$host.$fileurl.$timestamp.xml
     done
+  echo "Fetching $host/unused/rules"
+  curl -s -k "https://$panorama/api/?type=op&cmd=<show><jobs><id>$unused</id></jobs></show>&key=$key" | xmllint --format --recover - > $location$host.unused.$timestamp.xml
   done
   echo "Fetching $panorama/shared/address"
   curl -s -k "https://$panorama/api/?type=config&action=get&key=$key&xpath=/config/shared/address" | xmllint --format --recover - > $location$prefix.shared.address.$timestamp.xml
